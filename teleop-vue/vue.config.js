a687806e8d8f6b291fe92075c38ec678b58206dc
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2021-01-22 12:23:53 -0500

    Bootstrap 5 integration

---
+++ teleop-vue/vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    css: {
+        loaderOptions : {
+            css: {
+
+            },
+            postcss: {
+                postcssOptions : {
+                    plugins: function() {
+                        return [
+                            require('autoprefixer')
+                        ]
+                    }
+                }
+            },
+            scss: {
+                additionalData: `@import "~@/app.scss";`
+            }
+        }
+    }
+}
