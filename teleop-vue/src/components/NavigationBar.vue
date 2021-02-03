96e1a07bb220e2269305773e0ed686311de65e87
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2021-02-03 15:16:32 -0500

    Restructure OpenteraWrapper using Vue Router

--- teleop-vue/src/components/NavigationBar.vue
+++ teleop-vue/src/components/NavigationBar.vue
@@ -1,56 +1,60 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <router-link class="navbar-brand" v-bind:to="this.defaultPath">{{ this.name }}</router-link>
-            <button class="navbar-toggler" type="button" v-on:click="navToggler">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="flex-container navbar-collapse" v-show="this.showNav">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li v-for="link in links" v-bind:key="link.name">
-                        <router-link class="nav-link text-align-left" v-bind:to="link.path">{{ link.name }}</router-link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <div class="container-fluid">
+      <router-link class="navbar-brand" v-bind:to="this.defaultPath">{{this.name}}</router-link>
+      <button class="navbar-toggler" type="button" v-on:click="navToggler">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="flex-container navbar-collapse" v-show="this.showNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li v-for="link in links" v-bind:key="link.name">
+            <router-link
+              class="nav-link text-align-left"
+              v-bind:to="link">
+              {{ link.name }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: "navigation-bar",
-    data() {
-        return {
-            showNav: false
-        };
+  name: "navigation-bar",
+  data() {
+    return {
+      showNav: false
+    };
+  },
+  props: {
+    name: {
+      type: String,
+      default: "NavBar"
     },
-    props: {
-        name: {
-            type: String,
-            default: "NavBar"
-        },
-        links: {
-            type: Array,
-            required: true
-        },
-        defaultPath: {
-            type: String,
-            default : "/"
-        }
+    links: {
+      type: Array,
+      required: true
     },
-    methods: {
-        navToggler() {
-            this.showNav = this.showNav ? false : true;
-        }
+    defaultPath: {
+      type: String,
+      default: "/"
     }
-}
+  },
+  methods: {
+    navToggler() {
+      this.showNav = this.showNav ? false : true;
+    }
+  }
+};
 </script>
 
 <style>
 .flex-container {
-    display: flex;
+  display: flex;
 }
 .text-align-left {
-    text-align: left !important;
+  text-align: left !important;
 }
 </style>
--- teleop-vue/src/components/OpenteraWrapper.vue
+++ teleop-vue/src/components/OpenteraWrapper.vue
@@ -1,41 +1,24 @@
 <template>
   <div>
     <header>
-      <slot name="header" ></slot>
+      <slot name="header"></slot>
     </header>
     <main>
-      <router-view></router-view>
+      <router-view 
+        v-on:connect="connect"
+        v-on:call-all="callAll"
+        v-on:hang-up-all="hangUpAll">
+      </router-view>
     </main>
   </div>
-  <!-- <div>
-    <div class="shadow card bg-dark">
-      <div class="card-body">
-        <video-conference
-          overlay-video-id="overlayVideo"
-          v-bind:client-list="clientList"
-          v-bind:is-fullscreen="true"
-          v-bind:show-overlay-video="true"
-        >
-        </video-conference>
-      </div>
-    </div>
-    <button v-on:click="connect">Connect</button>
-    <button v-on:click="callAll">Call all</button>
-    <button v-on:click="hangUpAll">Hang up</button>
-  </div> -->
 </template>
 
 <script>
-//import VideoConference from "./VideoConference";
-
 import useLocalStream from "../composables/OpenteraWrapper/useLocalStream";
 import useStreamClient from "../composables/OpenteraWrapper/useStreamClient";
 
 export default {
   name: "opentera-wrapper",
-  components: {
-    //VideoConference
-  },
   props: {
     name: String,
     data: Object,
@@ -44,10 +27,7 @@ export default {
   setup(props) {
     const { localStream } = useLocalStream();
 
-    const { clientList, streamClient, connect } = useStreamClient(
-      props,
-      localStream
-    );
+    const { clientList, streamClient, connect } = useStreamClient(props, localStream);
 
     return {
       localStream,
