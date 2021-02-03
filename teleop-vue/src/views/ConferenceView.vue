b70dcc6ddf7eb6545d7af20a46235e2d3b6e581f
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2021-02-03 15:20:10 -0500

    Add ConferenceView component

---
+++ teleop-vue/src/views/ConferenceView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="shadow card bg-dark">
+      <div class="card-body">
+        <video-conference
+          overlay-video-id="overlayVideo"
+          v-bind:client-list="clientList"
+          v-bind:is-fullscreen="true"
+          v-bind:show-overlay-video="true"
+        >
+        </video-conference>
+      </div>
+    </div>
+    <button v-on:click="connect">Connect</button>
+    <button v-on:click="callAll">Call all</button>
+    <button v-on:click="hangUpAll">Hang up</button>
+  </div>
+</template>
+
+<script>
+import VideoConference from "../components/VideoConference";
+
+export default {
+    name: "conference-view",
+    components: {
+        VideoConference
+    },
+    emits : [
+        "connect",
+        "callAll",
+        "hangUpAll"
+    ],
+    methods: {
+        connect() {
+            this.$emit("connect");
+        },
+        callAll() {
+            this.$emit("callAll");
+        },
+        hangUpAll() {
+            this.$emit("hangUpAll");
+        }
+    },
+    computed: {
+        clientList() {
+            return this.$store.state.opentera.clientList;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
