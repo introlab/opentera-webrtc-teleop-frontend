f5b62649838d52000274e57c00a7c503eff393d7
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2021-02-18 15:58:07 -0500

    Add NavigationBar component

---
+++ teleop-vue/src/components/NavigationBar/NavigationBar.vue
@@ -0,0 +1,61 @@
+<template>
+    <nav class="navbar bg-dark">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" v-bind:to="defaultPath">{{brand}}</router-link>
+            <button class="icon-button navbar-toggler" type="button" v-on:click="navToggler">
+            <svg-icon class="navbar-toggler-icon" icon="list"></svg-icon>
+            </button>
+            <div class="flex-container navbar-collapse" v-show="showNav">
+                <ul ref="nav" class="navbar-nav">
+                    <li v-for="link in links" v-bind:key="link.name">
+                        <router-link
+                            class="nav-link"
+                            v-bind:to="link">
+                            {{link.name}}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>     
+        </div>
+    </nav>
+</template>
+
+<script>
+import { SvgIcon } from "@/components/SvgIcon";
+
+export default {
+    name: "navigation-bar",
+    components: {
+        SvgIcon
+    },
+    data() {
+        return {
+            showNav: false
+        }
+    },
+    props: {
+        brand: {
+            type: String,
+            required: true
+        },
+        links: {
+            type: Array,
+            required: true
+        },
+        defaultPath: {
+            type: String,
+            default: "/"
+        }
+    },
+    methods: {
+        navToggler() {
+            this.showNav = !this.showNav;
+        }
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "./NavigationBar.scss";
+</style>
