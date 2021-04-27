28c561c7d113c82a876288a7e6795cef80e393f5
Gabriel Lauzier <gabriel.lauzier@usherbrooke.ca>
2021-04-27 11:45:52 -0400

    Requested change on PR #11

--- teleop-vue/src/store/modules/opentera/index.ts
+++ teleop-vue/src/store/modules/opentera/index.ts
@@ -1,5 +1,6 @@
 export { StreamClientStore } from "./streamClientStore";
 export { DataChannelClientStore } from "./dataChannelClientStore";
+
 export * from "./types";
 export * from "./utils";
 export * from "./exceptions";
