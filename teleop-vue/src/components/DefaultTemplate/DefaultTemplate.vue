b57174235e813784c06d0b43c4ce319d947285a3
Jeremie Bourque <jeremie.bourque@usherbrooke.ca>
2021-09-21 11:30:36 -0400

    Message type for battery and signal

--- teleop-vue/src/components/DefaultTemplate/DefaultTemplate.vue
+++ teleop-vue/src/components/DefaultTemplate/DefaultTemplate.vue
@@ -55,16 +55,16 @@ export default {
     this.$store.commit(
       "localClient/openteraTeleop/setMessageEventHandler",
       (id, name, clientData, message) => {
-        // TODO
         const parsedMsg = JSON.parse(message);
-        if (parsedMsg.type === "status") {
+        if (parsedMsg.type === "batteryStatus") {
           this.$store.commit(
             "localClient/openteraTeleop/changeBatteryLevel",
-            parsedMsg.batteryLevel
+            parsedMsg.level
           );
+        } else if (parsedMsg.type === "signalStatus") {
           this.$store.commit(
             "localClient/openteraTeleop/changeSignalStrength",
-            parsedMsg.signalStrength
+            parsedMsg.strength
           );
         }
       }
