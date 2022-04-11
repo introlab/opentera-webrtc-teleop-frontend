95ab53eac11cda841af08deba5a81f398341b439
philippewarren <philippewarren31@gmail.com>
2022-04-11 10:00:40 -0400

    Replace magic constant with named constant

--- teleop-vue/src/views/robotCapabilities.ts
+++ teleop-vue/src/views/robotCapabilities.ts
@@ -1,5 +1,7 @@
+const DEFAULT_ROBOT_TYPE = "BEAM";
+
 export function robotCapabilities(robotType: string | null | undefined) {
-    robotType = robotType || "BEAM";
+    robotType = robotType || DEFAULT_ROBOT_TYPE;
     return {
         isRobotMobile: robotType != "TTOP",
         isRobotSingleCamera: robotType == "TTOP",
